88aa9c255d6a50acf09a7aea1d68aeb475dc4615
Aachman Mittal <m.aachman@gmail.com>
2024-04-01 11:51:01 +0530

    Add basic layout for main section and dummy data to js

---
+++ script.js
@@ -0,0 +1,86 @@
+const projects = [
+  {
+    title: "Super Cool Project",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce euismod nisl vel eros consectetur tempor a sit amet sem. Integer rhoncus dictum sem. Vestibulum et metus porta dolor molestie volutpat. Curabitur lorem elit, rutrum sed lorem vel, ornare lobortis tellus. Mauris ultricies imperdiet condimentum. Aliquam erat volutpat. ",
+  },
+  {
+    title: "Mildly Cool Project",
+    description:
+      "Nullam facilisis nec magna et iaculis. Fusce accumsan maximus nulla, quis fringilla magna bibendum sed. Sed ut blandit ex. Ut fringilla erat eget purus vestibulum, sed viverra orci pretium.",
+  },
+  {
+    title: "Project Super Hot",
+    description:
+      "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum elementum sem eget eros consectetur semper. Sed sed diam ac nisi porttitor elementum ac quis felis. Sed lorem tellus, efficitur in eros eu, volutpat semper nisl. In commodo arcu eget enim mattis, quis pulvinar diam iaculis. Suspendisse cursus turpis arcu, rhoncus tristique turpis faucibus sed. Proin vitae ligula et ex posuere gravida. Sed fermentum cursus justo sit amet volutpat. Donec in mi viverra, ultricies diam in, mollis tellus. In suscipit magna quam, sit amet sollicitudin justo tincidunt ac.",
+  },
+  {
+    title: "Sunshine Project",
+    description:
+      "Fusce in enim nunc. Morbi vel enim luctus, convallis diam id, lacinia felis. Donec sit amet quam quis velit mattis finibus. In hac habitasse platea dictumst.",
+  },
+  {
+    title: "Best Project",
+    description:
+      "Morbi sed scelerisque leo. In pellentesque eget eros eu ullamcorper. Donec pulvinar id metus eget tempus. Ut nec tempor nisl. Ut iaculis a justo ut blandit.",
+  },
+  {
+    title: "Always on HOLD",
+    description:
+      "Morbi dignissim eros lorem, in elementum dui placerat at. Phasellus pharetra viverra dolor in tempor. Etiam non odio et ex aliquam iaculis. Phasellus eu orci ligula. Nunc efficitur, mi sit amet porta tempus, enim ligula sollicitudin ipsum, nec dignissim nulla quam eget nisi. Nam auctor, ligula ac vestibulum sodales, sem purus malesuada mauris, sit amet condimentum nunc eros ac risus. Pellentesque placerat velit tempor turpis volutpat consequat. Aliquam facilisis purus ut gravida ultrices. Mauris scelerisque tincidunt neque, id feugiat lacus rhoncus nec. Nullam id ex id magna mattis euismod. Aliquam finibus ipsum vitae purus varius, in congue sapien tempor. Maecenas euismod felis id tellus varius, a fringilla augue porttitor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc egestas rhoncus massa vestibulum laoreet. Ut facilisis a nulla et bibendum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;",
+  },
+];
+
+const announcements = [
+  {
+    title: "Site Maintainence",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce euismod nisl vel eros consectetur tempor a sit amet sem. Integer rhoncus dictum sem.",
+  },
+  {
+    title: "Office Event",
+    description: "Vestibulum et metus porta dolor molestie volutpat. Curabitur lorem elit, rutrum sed lorem vel, ornare lobortis tellus.",
+  },
+  {
+    title: "Updated Privacy Policy",
+    description: "Mauris ultricies imperdiet condimentum.",
+  },
+];
+
+const trending = [
+  {
+    author: {
+      username: "tegan",
+      pfp: ""
+    },
+    project: {
+      name: "World Peach Builder"
+    }
+  },
+  {
+    author: {
+      username: "morgan",
+      pfp: ""
+    },
+    project: {
+      name: "Super Cool Project"
+    }
+  },
+  {
+    author: {
+      username: "kendall",
+      pfp: ""
+    },
+    project: {
+      name: "Life Changing App"
+    }
+  },
+  {
+    author: {
+      username: "Alex",
+      pfp: ""
+    },
+    project: {
+      name: "No traffic Maker"
+    }
+  }
+]
